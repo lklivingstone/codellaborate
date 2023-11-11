7f4f7701b91497141d14a5567f1ef0a6121cbe23
L K Livingstone <livingstone.lk.337@gmail.com>
2023-11-11 19:00:24 +0530

    First commit

---
+++ index2.js
@@ -0,0 +1,23 @@
+const { Server } = require("socket.io");
+const http = require("http");
+
+const server = http.createServer();
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+  socket.on("send-changes", (delta) => {
+    console.log("broadcasted");
+    socket.broadcast.emit("receive-changes", delta);
+  })
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Socket.io server is running on port ${PORT}`);
+});
